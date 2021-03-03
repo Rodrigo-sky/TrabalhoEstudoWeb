db6e86eb3f5502f819e90a3ad67b81dbeb190524
Skybudegas <rtrs.eu@gmail.com>
2021-03-03 20:47:26 -0300

    Up de arquivos para o github

---
+++ app.js
@@ -0,0 +1,61 @@
+const API_KEY = 'a6fac8ae05f349c34a524ec87df44498';
+
+// function exibeFilmes () {
+//     let divTela = document.getElementById('tela');
+//     let texto = '';
+
+//     // Montar texto HTML das noticias
+//     let dados = JSON.parse (this.responseText);
+//     for (i=0; i< dados.articles.length; i++) {
+//         let noticia = dados.articles[i];
+//         let data = new Date (noticia.publishedAt);
+
+//         texto = texto + `
+//             <div class="box-noticia">
+//                 <img src="${noticia.urlToImage}" alt="">
+//                 <span class="titulo">${noticia.title}</span><br>
+//                 <span class="creditos">${data.toLocaleDateString ()} - 
+//                     ${noticia.source.name} - 
+//                     ${noticia.author}</span><br>
+//                 <span class="text">
+//                 ${noticia.content} <a href="${noticia.url}">Leia mais ...</a>
+//                 </span>
+//             </div>            
+//         `;
+//     };
+
+//     // Preencher a DIV com o texto HTML
+//     divTela.innerHTML = texto;
+// }
+function caralha() { 
+    alert('funcionou')
+};
+
+function exibePopular(){
+    caralha();
+    let divPopulares = document.getElementById('telaPopular');
+    let texto = '';
+
+    let dadoPopular = JSON.parse(this.responseText);
+    for (i=0; i< dadoPopular.results.length; i++) {
+        let Popular = dadoPopular.results[i];
+        texto = texto + `
+        <div class="carousel-item active">
+            <img src="https://image.tmdb.org/t/p/w94_and_h141_bestv2${Popular.poster_path}" class="d-block w-100" alt="Descrição 1">
+        </div>
+        `
+
+    };
+    divPopulares.innerHTML = texto;
+
+};
+
+
+function testPopular() {
+    let xhr = new XMLHttpRequest();
+    xhr.onload = exibePopular();
+    xhr.open ('GET', `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=pt-BR&region=BR`);
+    xhr.send ();
+}
+
+document.getElementById ('btnSearch').addEventListener ('click', exibePopular);
---
+++ script.js
@@ -0,0 +1,116 @@
+const TMDB_ENDPOINT_BASE = 'https://api.themoviedb.org/3';
+
+// Função que cria os cards dos filmes
+function MostraFilmesEmCartaz () {
+    //Executar requisição AJAX
+
+    $.ajax({
+        // Passar a URL ENDPOINT BASE + /movie/now_playing
+        url: TMDB_ENDPOINT_BASE + '/movie/popular',
+        data: {
+            api_key: 'a6fac8ae05f349c34a524ec87df44498',
+            language: 'pt-BR'
+        }
+    })
+    // Receber o JSON
+    .done(function (data) {
+
+        let codigo_html = '';
+        
+         // Montar os cards
+         for (i=0; i < data.results.length; i++) {
+
+            // Concatenar o código do Card com os dados do JSON
+            titulo = data.results[i].title;
+            imagem = 'https://image.tmdb.org/t/p/w500' + data.results[i].poster_path;
+            descricao = data.results[i].overview;
+            id = data.results[i].id;
+
+            if(descricao == "") {
+                descricao = "Não há sinopse disponivel em pt-BR.<br>Para mais informações visite a pagina do The Movie Db";
+             }
+
+            codigo_html += `<div class="col mb-3">
+            <div class="card h-100">
+              <img src="${imagem}" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">${titulo}</h5>
+                <p class="card-text overflow-auto">${descricao}</p>
+                <a href="https://www.themoviedb.org/movie/${id}" class="btn btn-link">Saiba mais em The Movie Db</a>
+              </div>
+            </div>
+          </div>`;
+         }
+
+
+        // Repassar os cards para a página
+         $('#telaPopular').html(codigo_html);
+
+
+    }); 
+
+}
+
+
+
+function PesquisaFilmes () {
+    //Executar requisição AJAX
+
+    $.ajax({
+        
+        
+        // Passar a URL ENDPOINT BASE + /movie/now_playing
+        url: TMDB_ENDPOINT_BASE + '/search/movie',
+        data: {
+            api_key: 'a6fac8ae05f349c34a524ec87df44498',
+            language: 'pt-BR',
+            query: $('#txtSearch').html
+        }
+    })
+    
+    // Receber o JSON
+    .done(function (data) {
+
+        let codigo_html = '';
+        
+         // Montar os cards
+         for (i=0; i < 4; i++) {
+
+            // Concatenar o código do Card com os dados do JSON
+            titulo = data.results[i].title;
+            imagem = 'https://image.tmdb.org/t/p/w500' + data.results[i].poster_path;
+            descricao = data.results[i].overview;
+            id = data.results[i].id;
+
+            if(descricao == "") {
+                descricao = "Não há sinopse disponivel em pt-BR.<br>Para mais informações visite a pagina do The Movie Db";
+             }
+
+            codigo_html += `<div class="col mb-3">
+            <div class="card h-100">
+              <img src="${imagem}" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">${titulo}</h5>
+                <p class="card-text overflow-auto">${descricao}</p>
+                <a href="https://www.themoviedb.org/movie/${id}" class="btn btn-link">Saiba mais em The Movie Db</a>
+              </div>
+            </div>
+          </div>`;
+         }
+
+
+        // Repassar os cards para a página
+         $('#telaPesquisa').html(codigo_html);
+
+
+    }); 
+
+}
+
+
+$( document ).ready(function () {
+
+    MostraFilmesEmCartaz ();
+
+    $('#btnSearch').click (PesquisaFilmes);
+});
